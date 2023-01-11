f4725aa4ed715d0eb5a40a8e7e2a5672be46f58e
Joao Mota <joaoefmota@gmail.com>
2023-01-11 12:44:18 +0100

    setup 1

---
+++ apps/backend/src/app.ts
@@ -0,0 +1,19 @@
+import express from "express";
+import cors from "cors";
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("Hello there!");
+});
+
+app.listen(5000, (err: boolean) => {
+  if (err) {
+    console.error("Something bad happened");
+  } else {
+    // eslint-disable-next-line no-restricted-syntax
+    console.log(`Server is listening on port 5000`);
+  }
+});
