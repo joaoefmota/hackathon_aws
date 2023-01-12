5c7814396a4182369a24df7c9da429ae9c2ee730
Kukuruzka1 <irinakukuruza295@gmail.com>
2023-01-12 10:01:56 +0100

    edited configuration

--- packages/eslint-config-custom/index.js
+++ packages/eslint-config-custom/index.js
@@ -4,4 +4,10 @@ module.exports = {
     "@next/next/no-html-link-for-pages": "off",
     "react/jsx-key": "off",
   },
+  "prettier/prettier": [
+    "error",
+    {
+      endOfLine: "auto",
+    },
+  ],
 };
