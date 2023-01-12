b4bdb69c0aa6662f4d0bace568d63dbbf01fff1c
Joao Mota <joaoefmota@gmail.com>
2023-01-12 11:52:32 +0100

    tailwind setup done and images backup fetching by axios done at /home/

--- apps/web/pages/Home/[id]/index.tsx
+++ apps/web/pages/Home/[id]/index.tsx
@@ -2,6 +2,7 @@ import * as React from "react";
 import axios from "axios";
 import { useRouter } from "next/router";
 import { useEffect, useState } from "react";
+import Image from "next/image";
 
 const SingleVehicle = () => {
   const router = useRouter();
@@ -14,10 +15,38 @@ const SingleVehicle = () => {
     axios
       .get(`http://localhost:5000/api/vehicles/${id}`)
       .then((response) => response.data)
-      .then((result) => setContents(result[0]) & console.log(result[0]));
+      .then((result) => {
+        setContents(result[0]);
+        console.log(result[0]);
+      });
   }, [id]);
 
-  return <>{contents == null ? <p>Loading...</p> : <p>Do the thing</p>}</>;
+  const apiUrl = "http://localhost:5000";
+  const mainImage = contents.image_main
+    ? apiUrl.concat(contents.image_main)
+    : undefined;
+  const secondImage = contents.image_second
+    ? apiUrl.concat(contents.image_second)
+    : undefined;
+
+  return (
+    <>
+      {contents == null ? (
+        <p>Loading...</p>
+      ) : (
+        <>
+          <p>Do the thing</p>
+          <Image src={mainImage} alt="Vehicle Main" width={256} height={256} />
+          <Image
+            src={secondImage}
+            alt="Vehicle Main"
+            width={256}
+            height={256}
+          />
+        </>
+      )}
+    </>
+  );
 };
 
 export default SingleVehicle;
