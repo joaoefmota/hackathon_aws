1c1656719ebb45d07cca8be47200184ee5eb55d9
DiogoG8 <110616195+DiogoG8@users.noreply.github.com>
2023-01-12 09:01:04 +0000

    some changes

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,10 +1,10 @@
 module.exports = {
-  "parserOptions": {
-    "ecmaVersion": 6
-},
+  parserOptions: {
+    ecmaVersion: 6,
+  },
   root: true,
   // This tells ESLint to load the config from the package `eslint-config-custom`
-  // extends: ["custom"],
+  extends: ["next/babel", "next/core-web-vitals"],
   settings: {
     next: {
       rootDir: ["apps/*/"],
