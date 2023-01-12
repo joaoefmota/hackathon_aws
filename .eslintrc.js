5575403b91ea606384ac81b1043d187cde8ac149
DiogoG8 <110616195+DiogoG8@users.noreply.github.com>
2023-01-12 09:28:26 +0000

    more setup

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,7 +4,7 @@ module.exports = {
   },
   root: true,
   // This tells ESLint to load the config from the package `eslint-config-custom`
-  extends: ["next/babel", "next/core-web-vitals"],
+  //extends: ["next/babel", "next/core-web-vitals"],
   settings: {
     next: {
       rootDir: ["apps/*/"],
