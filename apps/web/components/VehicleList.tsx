f8fe52ca027976967958b0060a14cb7cfd8d450c
David Alexander Pfeiffer <david.pfeiffer971@gmail.com>
2023-01-12 14:59:46 +0100

    Scaffold vehicle list structure

---
+++ apps/web/components/VehicleList.tsx
@@ -0,0 +1,8 @@
+import React from 'react'
+
+// This is the list that maps vehicles into a VehicleListItem each
+export default function VehicleList() {
+  return (
+    <div>VehicleList</div>
+  )
+}
---
+++ apps/web/components/VehicleListItem.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function VehicleListItem() {
+  return (
+    // Contains header image of car
+    // Contains clickable link on its bottom
+    <div>ListItem</div>
+  )
+}
