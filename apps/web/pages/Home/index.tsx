c1e1928fc7467c5eaa96c85d688b2c3c88ac3bab
DiogoG8 <110616195+DiogoG8@users.noreply.github.com>
2023-01-12 13:18:44 +0000

    Frontend

--- apps/web/pages/Home/index.tsx
+++ apps/web/pages/Home/index.tsx
@@ -8,10 +8,34 @@ const Home = () => {
     <>
       {/*Header*/}
       <div className={styles["test"]}>
-        <button>Log In</button>
-        <button>Register</button>
-        <button></button>
+        <div className={styles["test5"]}>
+          <button className={styles["test4"]}></button>
+          <div className={styles["test6"]}></div>
+          <div className={styles["test2"]}>
+            <button className={styles["test1"]}>Log In</button>
+            <button className={styles["test1"]}>Register</button>
+          </div>
+        </div>
       </div>
+      {/*Header*/}
+      {/*Body*/}
+      <div className={styles["body"]}>
+        <div className={styles["bodytext"]}>Let’s vintaginate your journey</div>
+        <div className={styles["bodytext2"]}>
+          Choose your perfect vintage car
+        </div>
+      </div>
+      {/*Body*/}
+      {/*Footer*/}
+      <div className={styles["footer"]}>
+        <div className={styles["footercontainer"]}>
+          <div className={styles["footertext"]}>HELLO@VINTAGE.DE</div>
+          <div className={styles["footertext"]}> (478) 458-4258</div>
+        </div>
+        <div className={styles["footerig"]}></div>
+        <div className={styles["footerfb"]}></div>
+      </div>
+      {/*Footer*/}
     </>
   );
 };
